3c51cbd4c39b8e1ca9066e132837f6a0b7102456
Aditya Jawanjal <adityajawanjal01@gmail.com>
2023-07-31 11:07:30 +0530

    o

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const router = require("./routes");
 const connectDB = require("./db/db");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,12 @@ app.use(cors());
 app.use('/api',router);
 
 connectDB();
+
+app.use(express.static(path.join(__dirname, "/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/dist/index.html"));
+})
 
 const port = process.env.PORT;
 app.listen(port , ()=>{
